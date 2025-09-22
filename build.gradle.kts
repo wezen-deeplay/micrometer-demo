ce759f6ed8525289b96598da10192079cd8d13ae
Wezen <wezen@deeplay.io>
2025-09-22 07:52:02 +0600

    chore: added shadow application and declaration main-class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     id("java")
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "io.deeplay.wezzen.demo"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("io.deeplay.wezzen.demo.runner.Runner")
+}
 
 repositories {
     mavenCentral()
@@ -21,4 +27,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveBaseName.set("MicrometerDemo")
+    archiveVersion.set("1.0-SNAPSHOT")
+    manifest {
+        attributes["Main-Class"] = "io.deeplay.wezzen.demo.runner.Runner"
+    }
 }
